5bae5b9b7241dbe0ae4667d0620ad12e203a4c82
mevawalac <chintu.m18@gmail.com>
2017-06-17 13:25:03 +0530

    Project Files moved to Different Repository

--- Events.js
+++
@@ -1,8 +0,0 @@
-var events = require("events")
-
-var myEmitter = new events.EventEmitter();
-myEmitter.on("someEvent", function(msg) {
-  console.log(msg);
-});
-
-myEmitter.emit("someEvent", "this event is emitter by myEventEmitter")
