5fa1dd78fedc5d979e2c9f9a3aa30af349344d75
mevawalac <chintu.m18@gmail.com>
2017-06-24 16:06:10 +0530

    Added CRUD Operations using MongoDB

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,34 +1,70 @@
 var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://admin:admin@ds135532.mlab.com:35532/todolist');
 
-module.exports = function(app) {
+// Create Schema
+var todoschema = new mongoose.Schema({
+  item: String
+});
 
-var data = [{
-    item: "Sachin Tendular"
-},
-{
-    item: "Virendar Shewag"
-}]
+// Create Model
+var TodoModel = mongoose.model("Todo", todoschema);
+
+// Save Item in Model
+// var firstItem = TodoModel({ item: "Sachin Tendular"}).save(function(error) {
+//   if(error) throw error;
+//
+//   console.log("Item Saved");
+// })
+
 
 var urlencodedparser = bodyparser.urlencoded({ extended: false}); // ??
 
+
+module.exports = function(app) {
+
 app.get('/todo', function(request, response) {
-  response.render("todo", {todos: data});
+  // Get todos from MongoDB
+  TodoModel.find({}, function(error, data) {
+    if(error) throw error
+
+    response.render("todo", {todos: data});
+  });
+
+  // Im Memory
+  // response.render("todo", {todos: data});
 });
 
 app.post('/todo', urlencodedparser, function(request, response) {
-  data.push(request.body);
-  response.json(data);
-  // Variants
-  //        response.send() // ?
-  //        response.render() // ?
+  // Insert todo into MongoDB
+  var newTodo = TodoModel(request.body).save(function(error, data){
+    if(error) throw error
+
+    response.json(data);
+    // Variants
+    //     response.send() // ?
+    //     response.render() // ?
+  });
+
+  // In Memory
+  // data.push(request.body)
+  // response.json(data);
 });
 
 app.delete('/todo/:item', function(request, response) {
-  data = data.filter(function(todo) {
-    return todo.item.replace(/ /g, '-') !== request.params.item;
+  // Delete todo from MongoDB
+  TodoModel.find({item: request.params.item.replace(/\-/g, ' ')}).remove(function(error, data) {
+    if(error) throw error
+
+    response.json(data);
   });
 
-  response.json(data); // ?
+  // In Memory
+  // data = data.filter(function(todo) {
+  //   return todo.item.replace(/ /g, '-') !== request.params.item;
+  // });
+  //
+  // response.json(data); // ?
 });
 
 };
