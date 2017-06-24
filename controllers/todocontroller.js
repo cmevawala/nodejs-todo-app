4a35f5a29e35f4fb15c7fa778bdea4fccb654a27
mevawalac <chintu.m18@gmail.com>
2017-06-24 14:49:26 +0530

    Completed Todo CRUD in Memory

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,15 +1,34 @@
-module.exports = function(app){
+var bodyparser = require('body-parser');
+
+module.exports = function(app) {
+
+var data = [{
+    item: "Sachin Tendular"
+},
+{
+    item: "Virendar Shewag"
+}]
+
+var urlencodedparser = bodyparser.urlencoded({ extended: false}); // ??
 
 app.get('/todo', function(request, response) {
-  // Todo..
+  response.render("todo", {todos: data});
 });
 
-app.post('/todo', function(request, response) {
-  // Todo..
+app.post('/todo', urlencodedparser, function(request, response) {
+  data.push(request.body);
+  response.json(data);
+  // Variants
+  //        response.send() // ?
+  //        response.render() // ?
 });
 
-app.delete('/todo', function(request, response) {
-  // Todo..
+app.delete('/todo/:item', function(request, response) {
+  data = data.filter(function(todo) {
+    return todo.item.replace(/ /g, '-') !== request.params.item;
+  });
+
+  response.json(data); // ?
 });
 
 };
